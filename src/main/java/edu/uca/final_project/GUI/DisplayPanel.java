c14e9100ee653d610fd39e9cb62e5e8ddad536dd
luke-j-carter <123020722+luke-j-carter@users.noreply.github.com>
2024-11-14 16:22:39 -0600

    Added the details and table panels to the display panel
    Feel free to change the layout, I thought that the details would look better below the table rather than beside it.

--- src/main/java/edu/uca/final_project/GUI/DisplayPanel.java
+++ src/main/java/edu/uca/final_project/GUI/DisplayPanel.java
@@ -12,6 +12,13 @@ public class DisplayPanel extends JPanel {
 
     public DisplayPanel(TableModel model) {
         System.out.println("Inside DisplayPanel");
+
+        DetailsPanel detailsPanel = new DetailsPanel();
+        TablePanel tablePanel = new TablePanel(model, detailsPanel);
+
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS)); //Places the detailsPanel below the table
+        add(tablePanel); // Add TablePanel to DisplayPanel
+        add(detailsPanel); // Add DetailsPanel to DisplayPanel
         add(new TablePanel(model));
 //        setVisible(true);
     }
